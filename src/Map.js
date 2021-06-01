5ba7aebe0b69ca24c76f3acce00844ec54ba0961
astridx <astrid.guenther@dimna.de>
2021-06-01 11:26:00 +0200

    wip

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
+import { useDispatchMap } from "./hooks/mapHook";
+import { MarkerList } from "./Marker/MarkerList";
 
 export const Map = () => {
+  const mapDispatch = useDispatchMap();
   const [mapViewport, setMapViewport] = useState({
     height: "100vh",
     width: "100wh",
@@ -15,6 +18,12 @@ export const Map = () => {
       {...mapViewport}
       mapStyle="https://api.maptiler.com/maps/streets/style.json?key=my_key"
       onViewportChange={setMapViewport}
-    ></ReactMapGL>
+      onClick={x => {
+        x.srcEvent.which === 1 &&
+          mapDispatch({ type: "ADD_MARKER", payload: { marker: x.lngLat } });
+      }}
+    >
+        <MarkerList />
+    </ReactMapGL>
   );
 };
