4c71983d03c8f34b5fc2da01842a74038bbc0d24
astridx <astrid.guenther@dimna.de>
2021-06-01 12:27:16 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; ReactDOM.render(<></>, document.getElementById('root'));
+import ReactDOM from 'react-dom'; 
+
+ReactDOM.render(<></>, document.getElementById('root'));
