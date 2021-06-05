12a4cb625cca2b71c89c3f857156ace857bd2612
astridx <astrid.guenther@dimna.de>
2021-06-05 22:41:10 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -7,32 +7,32 @@ function getTitle(title) {
 
 const pois = [
   {
-    display_name:'Brot und mehr',
-    icon:'https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png',
-    lat:'50.4', 
-    lon:'7.1',
-    place_id:'1'
+    display_name: "Brot und mehr",
+    icon: "https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png",
+    lat: "50.4",
+    lon: "7.1",
+    place_id: "1",
   },
   {
-    display_name:'Baecker Mueller',
-    icon:'https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png',
-    lat:'51.4', 
-    lon:'6.1',
-    place_id:'2'
+    display_name: "Baecker Mueller",
+    icon: "https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png",
+    lat: "51.4",
+    lon: "6.1",
+    place_id: "2",
   },
   {
-    display_name:'Brot am Morgen',
-    icon:'https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png',
-    lat:'50.4', 
-    lon:'7.1',
-    place_id:'3'
+    display_name: "Brot am Morgen",
+    icon: "https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png",
+    lat: "50.4",
+    lon: "7.1",
+    place_id: "3",
   },
   {
-    display_name:'Baecker Pleinen',
-    icon:'https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png',
-    lat:'51.4', 
-    lon:'6.1',
-    place_id:'4'
+    display_name: "Baecker Pleinen",
+    icon: "https://nominatim.openstreetmap.org/ui/mapicons//shopping_bakery.p.20.png",
+    lat: "51.4",
+    lon: "6.1",
+    place_id: "4",
   },
 ];
 
