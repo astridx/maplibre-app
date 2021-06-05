8fa1b7c1d4e9b2d75d5f21d8dbc092a10b08807a
astridx <astrid.guenther@dimna.de>
2021-06-05 13:45:13 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
+const title = "React Maplibre Map";
+
 function App() {
-  const title = "React Maplibre Map";
   return (
     <div>
       <h1>React Maplibre Map</h1>
