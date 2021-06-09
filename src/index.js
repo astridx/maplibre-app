cfb8f0f96a3b70dd92bb3bfeadfc93fffc04c21b
astridx <astrid.guenther@dimna.de>
2021-06-09 21:35:22 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import ReactMapGL from "react-map-gl";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const mapViewport = {
+  height: "100vh",
+  width: "100vw",
+  longitude: 7.571606,
+  latitude: 50.226913,
+  zoom: 4,
+};
+
+const mapstyle =
+  "https://api.maptiler.com/maps/streets/style.json?key=" +
+  process.env.REACT_APP_MAPTILER_TOKEN;
+
+ReactDOM.render(
+  <ReactMapGL {...mapViewport} mapStyle={mapstyle}></ReactMapGL>,
+  document.getElementById("root")
+);
